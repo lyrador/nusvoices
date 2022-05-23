4d0fb880bc911e57d2c0c30e36f7bcc75636fd6a
lyrador <yeedaryl700@gmail.com>
2022-05-23 12:21:16 +0800

    video fix 2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='/videos/video-4.mp4' autoPlay loop muted playsInline/>
+        <video src='/videos/video-5.mp4' loop autoPlay muted playsInline/>
         <h1>HEAR IT. SEE IT. LIVE IT.</h1>
         {/* <h2>NUS' Premier Mandopop Group</h2> */}
         <p>What are you waiting for?</p>
@@ -27,7 +27,7 @@ function HeroSection() {
                     buttonStyle='btn--primary'
                     buttonSize='btn--large'
                 >
-                    WATCH TEASER
+                    WATCH TEASER <i className='far fa-play-circle'/>
                 </Button>
             </Link>
         </div>
