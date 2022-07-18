c64737ab1e73f744df3d36103b7afbb2c134ee4a
lyrador <yeedaryl700@gmail.com>
2022-07-18 23:24:55 +0800

    Changed Img Caption

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,7 +9,7 @@ function CardItem(props) {
           <li className='cards__item'>
           <a target="_blank" rel="noopener noreferrer" href={props.path} className='cards__item__link'>
                 <figure className='cards__item__pic-wrap' data-category={props.label}>
-                    <img src={props.src} alt='Travel Image' className='cards__item__img'/>
+                    <img src={props.src} alt='Voices' className='cards__item__img'/>
                 </figure>
                 <div className='cards__item__info'>
                     <h5 className='cards__item__text'>{props.text}</h5>
